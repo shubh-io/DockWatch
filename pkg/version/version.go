b90333ec466442110f9c6f7449ca6e457962b19f
SomeRandomGuy <factazone2020@gmail.com>
2025-12-11 17:54:46 +0530

    feat: add Homebrew tap support with macOS builds

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 // Current application version, would update this when releasing a new version.
-const Dockmate_Version = "0.0.4"
+const Dockmate_Version = "0.0.5"
 
 // Repository to check for releases (owner/repo)
 const Repo = "shubh-io/dockmate"
