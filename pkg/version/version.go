ee0e6a2b1357d789216c9c59f7b0d56a0514d049
SomeRandomGuy <factazone2020@gmail.com>
2025-12-26 15:12:46 +0530

    version: 0.0.17

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 // Current application version, would update this when releasing a new version.
-const Dockmate_Version = "0.0.16"
+const Dockmate_Version = "0.0.17"
 
 // Repository to check for releases (owner/repo)
 const Repo = "shubh-io/dockmate"
