569337a2e0bddef3c00236f8a15d9fa2b469732b
SomeRandomGuy <factazone2020@gmail.com>
2026-01-05 17:31:24 +0530

    feat: update application version to 0.1.0

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 // Current application version, would update this when releasing a new version.
-const Dockmate_Version = "0.0.19"
+const Dockmate_Version = "0.1.0"
 
 // Repository to check for releases (owner/repo)
 const Repo = "shubh-io/dockmate"
